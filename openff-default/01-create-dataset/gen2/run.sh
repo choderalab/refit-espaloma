766c6e49f51f767105d8fca0294889d6c9ec4ee7
kt <kntkb.qbr@gmail.com>
2023-03-13 15:12:33 -0400

    add scripts for gen2

---
+++ openff-default/01-create-dataset/gen2/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+DATASET_PATH=/home/takabak/data/qca-dataset/openff-default/gen2
+python check_status.py --hdf5 ${DATASET_PATH}/GEN2-OPTIMIZATION-DATASET-OPENFF-DEFAULT.hdf5 > mylist
+DIR=${PWD}
+
+x=`head -n 1 mylist | wc -l`
+if [ $x -eq 0 ]; then
+    echo "finished"
+    rm mylist
+    touch finished
+    exit
+fi
+
+while read line
+do
+    idx=`echo $line | awk '{print $1}'`
+    keyname=`echo $line | awk '{print $2}'`
+    #echo $idx $keyname
+
+    # create directory
+    mkdir -p data/${idx}
+    sed -e 's/@@@JOBNAME@@@/id-'${idx}'/g' \
+        -e 's/@@@KEYNAME@@@/'${keyname}'/g' \
+        -e 's/@@@INDEX@@@/'${idx}'/g' \
+        submit_template.sh > ./data/${idx}/submit.sh
+    chmod u+x ./data/${idx}/submit.sh
+
+    # submit job
+    cd data/${idx}
+    echo "submit job id-${idx}"
+    bsub < submit.sh
+    cd ${DIR}
+
+    #sleep 1
+done < mylist
