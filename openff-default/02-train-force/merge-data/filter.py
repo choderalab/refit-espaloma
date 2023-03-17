4363a4efd8635d0944a462c6cb85429bfb983674
kt <kntkb.qbr@gmail.com>
2023-03-17 14:26:56 -0400

    update comments

--- openff-default/02-train-force/merge-data/filter.py
+++ openff-default/02-train-force/merge-data/filter.py
@@ -8,13 +8,13 @@ import torch
 import espaloma as esp
 
 
-
+# ---
 # Basic settings
+# ---
 BASE_FORCEFIELD = "openff-2.0.0"
 MAX_ENERGY = 0.1   # hartee (62.75 kcal/mol)
 MAX_ENERGY_DIFFERENCE = 0.065   # hartee (40.7875 kcal/mol)
 HARTEE_TO_KCALPERMOL = 627.5
-#MIN_CONF_RATIO = 0.2
 MIN_CONF = 5
 
 
@@ -32,85 +32,75 @@ def run(kwargs):
     with open("calc_ff_{}_filtered.log".format(dataset), "w") as wf:
         wf.write(">{}: {} molecules found\n".format(dataset, n_total_mols))
         for p in paths_to_mydata:
-            _g = esp.Graph.load(p)   # graph generated from hdf5 file. u_ref corresponds to native QM energy.
+            _g = esp.Graph.load(p)
             g = copy.deepcopy(_g)
             n_confs = g.nodes['n1'].data['xyz'].shape[1]
+            
+            #
+            # Filter high energy conformers and qm/mm inconsistant molecules
+            #
 
-
-            """
-            filter high energy conformers and qm/mm inconsistant molecules
-            """
-            # 1: relative qm energy
+            # Relative qm energy
             index1 = g.nodes['g'].data['u_qm'] <= g.nodes['g'].data['u_qm'].min() + MAX_ENERGY
             index1 = index1.flatten()
             #print(index1)
 
-            # 2: relative qm energy after nonbonded interactions are subtracted
+            # Relative qm energy after nonbonded interactions are subtracted
             index2 = g.nodes['g'].data['u_ref'] <= g.nodes['g'].data['u_ref'].min() + MAX_ENERGY
             index2 = index2.flatten()
             #print(index2)
             
-            # 3: relative legacy ff energy
+            # Relative baseline ff energy
             index3 = g.nodes['g'].data['u_%s' % BASE_FORCEFIELD] <= g.nodes['g'].data['u_%s' % BASE_FORCEFIELD].min() + MAX_ENERGY
             index3 = index3.flatten()
             #print(index3)
 
-            # 4: energy difference between qm and legacy force field
+            # Energy difference between qm and baseline ff
             _u = g.nodes['g'].data['u_%s' % BASE_FORCEFIELD] - g.nodes['g'].data['u_%s' % BASE_FORCEFIELD].min()
             _u_qm = g.nodes['g'].data['u_qm'] - g.nodes['g'].data['u_qm'].min()
-            index4 = abs(_u - _u_qm) <= MAX_ENERGY_DIFFERENCE  # hartee
+            index4 = abs(_u - _u_qm) <= MAX_ENERGY_DIFFERENCE
             index4 = index4.flatten()
             #print(index4)
 
-            # logical
+
+            #
+            # Get valid conformations that passed the filtering
+            #
             index12 = torch.logical_and(index1, index2)
             index34 = torch.logical_and(index3, index4)
             index = torch.logical_and(index12, index34)
             #print(index)
 
-            # count valid conformers
-            n_valid_confs += np.array(index, dtype=int).sum()
-            n_total_confs += n_confs
-
-
-            """
-            grab valid conformations only
-            """
             for key in g.nodes['g'].data.keys():
                 if key.startswith('u_'):    
                     g.nodes['g'].data[key] = g.nodes['g'].data[key][:, index]            
             g.nodes['n1'].data['u_ref_prime'] = g.nodes['n1'].data['u_ref_prime'][:, index, :]
             g.nodes['n1'].data['xyz'] = g.nodes['n1'].data['xyz'][:, index, :]
 
-            # calculate relative u_ref energy            
+            # Calculate relative u_ref energy by subtracting mean
             g.nodes['g'].data['u_ref_relative'] = g.nodes['g'].data['u_ref'].detach().clone()
             g.nodes['g'].data['u_ref_relative'] = g.nodes['g'].data['u_ref_relative'] - g.nodes['g'].data['u_ref_relative'].mean(dim=-1, keepdims=True)
 
 
-            """
-            report
-            """
-            entry_id = p.split('/')[-1]  # str
+            #
+            # Export
+            #
+            n_valid_confs += np.array(index, dtype=int).sum()
+            n_total_confs += n_confs
+            entry_id = int(p.split('/')[-1])
             n_passed_confs = np.array(index, dtype=int).sum()
+
             if n_passed_confs == n_confs:
-                wf.write("{:8d}: {:4d} / {:4d} conformations passed the filter\n".format(int(entry_id), n_passed_confs, n_confs))
-            #elif n_passed_confs/n_confs < MIN_CONF_RATIO:
+                wf.write("{:8d}: {:4d} / {:4d} conformations passed the filter\n".format(entry_id, n_passed_confs, n_confs))
             elif n_passed_confs < MIN_CONF:
-                wf.write("{:8d}: {:4d} / {:4d} conformations passed the filter. Number of valid conformations did not pass the threshold. (excluded all)\n".format(int(entry_id), n_passed_confs, n_confs))
+                wf.write("{:8d}: {:4d} / {:4d} conformations passed the filter. Number of valid conformations did not pass the threshold. (excluded all)\n".format(entry_id, n_passed_confs, n_confs))
             else:
-                wf.write("{:8d}: {:4d} / {:4d} conformations passed the filter ({} excluded)\n".format(int(entry_id), n_passed_confs, n_confs, n_confs - n_passed_confs))
+                wf.write("{:8d}: {:4d} / {:4d} conformations passed the filter ({} excluded)\n".format(entry_id, n_passed_confs, n_confs, n_confs - n_passed_confs))
 
-
-            """
-            save graph
-            """
-            #if n_passed_confs/n_confs >= MIN_CONF_RATIO:
             if n_passed_confs >= MIN_CONF:
                 g.save('{}/{}/{}'.format(BASE_FORCEFIELD + "_filtered", dataset, entry_id))
                 n_valid_mols += 1
 
-
-        # summary
         wf.write("------------------\n")
         wf.write(">total molecules: {}\n".format(n_total_mols))
         wf.write(">total conformations: {}\n".format(n_total_confs))
---
+++ openff-default/02-train-force/merge-data/filter.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#BSUB -P "filter"
+#BSUB -J "filter"
+#BSUB -n 1
+#BSUB -R rusage[mem=8]
+#BSUB -R span[hosts=1]
+#BSUB -q cpuqueue
+#BSUB -sp 1 # low priority. default is 12, max is 25
+#BSUB -W 24:00
+#BSUB -o filter_%J_%I.stdout
+#BSUB -eo filter_%J_%I.stderr
+#BSUB -L /bin/bash
+
+
+source ~/.bashrc
+OPENMM_CPU_THREADS=1
+export OE_LICENSE=~/.openeye/oe_license.txt   # Open eye license activation/env
+
+
+# change dir
+echo "changing directory to ${LS_SUBCWD}"
+cd $LS_SUBCWD
+
+
+# Report node in use
+echo "======================"
+hostname
+#env | sort | grep 'CUDA'
+#nvidia-smi
+echo "======================"
+
+
+# run job
+#conda activate espaloma-dev
+conda activate espaloma
+
+# rna
+#dataset="rna-diverse-trinucleotide"
+#mkdir -p openff-2.0.0_filtered/${dataset}
+#python filter.py --dataset ${dataset}
+
+# rna-trinucleotide
+#dataset="rna-trinucleotide"
+#mkdir -p openff-2.0.0_filtered/${dataset}
+#python filter.py --dataset ${dataset}
+
+# rna-nucleoside
+dataset="rna-nucleoside"
+mkdir -p openff-2.0.0_filtered/${dataset}
+python filter.py --dataset ${dataset}
+
+# spice-pubchem
+#dataset="spice-pubchem"
+#mkdir -p openff-2.0.0_filtered/${dataset}
+#python filter.py --dataset ${dataset}
+
+# spice-dipeptide
+#dataset="spice-dipeptide"
+#mkdir -p openff-2.0.0_filtered/${dataset}
+#python filter.py --dataset ${dataset}
+
+# gen2
+#dataset="gen2"
+#mkdir -p openff-2.0.0_filtered/${dataset}
+#python filter.py --dataset ${dataset}
+
+# pepconf
+#dataset="pepconf"
+#mkdir -p openff-2.0.0_filtered/${dataset}
+#python filter.py --dataset ${dataset}
+
+# spice-des-monomers
+#dataset="spice-des-monomers"
+#mkdir -p openff-2.0.0_filtered/${dataset}
+#python filter.py --dataset ${dataset}
