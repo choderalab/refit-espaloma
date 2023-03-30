fb33d86a7e8d5e137105edaf1d3cb3bddc1d97d0
kt <kntkb.qbr@gmail.com>
2023-03-29 22:00:24 -0400

    remove 01-create-dataset/gen2

--- openff-default/01-create-dataset/gen2/move_entry600_to_data_failure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-# Move problematic molecule manually. This moleucle will raise error when filtering and calculating energies with legacy force fields.
-# 600 Cc1ccc2c(c1)[C@@H]([C@@H](C2)C)Nc3ncc(cn3)C#C[Si](C)(C)C
-
-mv data/600 data.failure/600
